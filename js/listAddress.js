3f5705a964046db0b783b717043eb2e5cd4a569c
dantegituser <dedisainer@gmail.com>
2020-09-04 14:53:42 -0500

    antes de intro

--- js/listAddress.js
+++ js/listAddress.js
@@ -1,36 +1,35 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     var liHover = document.querySelectorAll('.list-address li');
 
 
     liHover.forEach(element => {
 
         element.addEventListener('mouseover', () => {
-        let liId = element.getAttribute('data-id');
+            let liId = element.getAttribute('data-id');
 
-        aparecerImg(liId);
-        })
+            aparecerImg(liId);
+        });
+    });
+
+    var w = window.innerWidth;
+    //console.log(w)
+    if (w < 780) {
+        liHover.forEach(element => {
+
+            element.addEventListener('click', () => {
+                let liId = element.getAttribute('data-id');
+                console.log('mobil');
+                aparecerImg(liId);
+            });
+        });
+    }
+
+
+    function aparecerImg(id) {
+
+        let imagen = document.querySelector('.imagen-arch img');
+        imagen.src = `img/list${id}.jpg`;
+
+    }
 });
-
-var w = window.innerWidth;
-//console.log(w)
-if(w < 780){
-    liHover.forEach(element => {
- 
-        element.addEventListener('click', () => {
-        let liId = element.getAttribute('data-id');
-console.log('mobil');
-        aparecerImg(liId);
-        })
-});
-}
-
-
-function aparecerImg( id ) {
-
-    let imagen = document.querySelector('.imagen-arch img');
-    imagen.src = `img/list${id}.jpg`;
-  
-}
-})
